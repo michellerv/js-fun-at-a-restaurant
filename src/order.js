b91581ae3a17b9c43fe3918ca964ead756b4ffd6
Michelle Villagran <mrvillagran16@gmail.com>
2023-05-12 17:36:05 -0600

    Add funtions for order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(orders, deliveryOrders) {
+if (deliveryOrders.length <= 2) 
+  return deliveryOrders.push(orders)
+}
+
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for ( var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1)
+      break;
+    } 
+  }
+  }
+
+function listItems(deliveryOrders) {
+ var array = []
+for (var i = 0; i < deliveryOrders.length; i++){
+   array.push(deliveryOrders[i].item)
+  } 
+  return array.join(', ')
+}
+
+function searchOrder(deliveryOrders, item) {
+var array = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    array.push(deliveryOrders[i].item)
+    array.join(',') } {
+if (array.includes(item)) {
+  return true
+  } else {
+    return false
+  }
+  }
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
