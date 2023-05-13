94fed0c386972743097bf259b6ab01ee9f2d782e
Michelle Villagran <mrvillagran16@gmail.com>
2023-05-13 12:15:45 -0600

    Add createResturant function. four of five tests pass

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,18 @@
-
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+    types: {
+      breakfast:[],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
+}
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem,
   // checkForFood
