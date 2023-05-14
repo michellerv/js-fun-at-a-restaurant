b361adbbb7fe51881df201b65501813668ff7733
Michelle Villagran <mrvillagran16@gmail.com>
2023-05-14 15:28:45 -0600

    Add removeMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,15 +11,25 @@ function createRestaurant(name) {
 
 
 function addMenuItem(resName, item) {
-if (!resName.menus[item.type].includes(item))
- {
-  return resName.menus[item.type].push(item)
+  if (!resName.menus[item.type].includes(item))
+  {
+    return resName.menus[item.type].push(item)
 } 
 }
 
-function removeMenuItem() {
-  
+function removeMenuItem(resName, item, orderT) {  
+  var menus = resName.menus[orderT]
+
+  for ( var i = 0; i < menus.length; i++) {
+    if (menus[i].name === item) {
+      menus.splice(i, 1) 
+      return `No one is eating our ${item} - it has been removed from the ${orderT} menu!`
+    } 
+  }
+  return `Sorry, we don\'t sell ${item}, try adding a new recipe!`
 }
+
+
 
 module.exports = {
   createRestaurant, 
